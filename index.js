e258fee57e51ab2c09be5a756fbd6ec693e4ce84
danielnels <danieln@newground.net.au>
2022-02-12 10:16:49 +0800

    Chaned function to inculde path

--- index.js
+++ index.js
@@ -1,83 +1,67 @@
-// this requires/calls inquirer
-const inquirer = require('inquirer');
-// this requires the generateMarkdonw in ifle
-const generateMarkdown = require('./utils/generateMarkdown.js')
-// this requires/calls file system
-const fs = require('fs')
-// array of questions for user
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+const createMarkdown = require("./utils/generateMarkdown.js");
+
 const questions = [
     {
-        type: 'input',
-        name: 'Title',
-        message: 'Please provide the title of your project?',
+        type: "input",
+        name: "enterGitHubUsername",
+        message: "What is your GitHub username?"
     },
     {
-        type: 'input',
-        name: 'Description',
-        message: 'Please provide a description of your project?',
+        type: "input",
+        name: "enterYourEmail",
+        message: "What is your email?"
     },
     {
-        type: 'checkbox',
-        name: 'Contents',
-        choices: ['Title', 'Description', 'Table of Contents', 'Installations', 'Usage', 'License', 'Contributors', 'Testing', 'GitHub Repo', 'Email for question inquiries'],
-        message: 'Please select the bullet points you would like to include in the Table of Contents?',
-
+        type: "input",
+        name: "enterProjectName",
+        message: "What is your project name?"
     },
     {
-        type: 'input',
-        name: 'Installation',
-        message: 'Please provide required installations for this application?',
+        type: "input",
+        name: "enterDescription",
+        message: "What is the description of your project?"
     },
     {
-        type: 'input',
-        name: 'Usage',
-        message: 'Please provide the usage instructions?',
+        type: "list",
+        name: "License",
+        message: "What is your license?",
+        choices: ["MIT", "Apache", "GNU"]
     },
     {
-        type: 'list',
-        name: 'License',
-        choices: ['Apache License 2.0', ' MIT License', 'Eclipse Public License 2.0', 'Mozilla Public License 2.0'],
-        message: 'Select the type of licese.',
+        type: "input",
+        name: "Installation",
+        message: "How to install dependencies for the project"
     },
     {
-        type: 'input',
-        name: 'Contributors',
-        message: 'Please provide the application contributing members.'
+        type: "input",
+        name: "Usage",
+        message: "What will the project be used for?"
     },
     {
-        type: 'input',
-        name: 'Test',
-        message: 'Please provide testing instructions for this application.'
+        type: "input",
+        name: "Contributing",
+        message: "What will your project contribute?"
     },
     {
-        type: 'input',
-        name: 'Github',
-        message: 'Please provide your GitHub username.',
-    },
-    {
-        type: 'input',
-        name: 'Email',
-        message: 'Please provide your email for inquiries regarding your application.',
+        type: "input",
+        name: "Tests",
+        message: "How do you run tests for the project?"
     }
-];
+]
 
-// function to write README file
-function writeToFile(fileName, data) {
-
-    return fs.writeFileSync(fileName, data)
+function promptQuestions(){
+    inquirer.prompt(questions).then((responses) => {
+        console.log(responses);
+        writeResponse("README.md", createMarkdown({
+            ...responses
+        }));
+    })
 }
+promptQuestions();
 
-// function to initialize program
-function init() {
-    // initiates questions object prompt
-    inquirer.prompt(questions)
-        // writes file to READMEGENERATED.md using data from generateMarkdown
-        .then((data) => writeToFile('READMEGENERATED.md', generateMarkdown(data)))
-        // once printed w/o erros succesful comand  is printed
-        .then(() => console.log("README successfully written."))
-        // this will print any erros in the command line, if any.
-        .catch((err) => console.log(err))
+function writeResponse(fileName, data){
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
-
-// function call to initialize program
-init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,48 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// // TODO: Create a function that returns a license badge based on which license is passed in
+// // If there is no license, return an empty string
+// function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) {}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ![GitHub license](https://img.shields.io/badge/license-${data.License}-blue.svg)
+  ## description
+  ${data.enterDescription}
+  ## tableOfContents
+  * [title](#title)
+  ## description
+  * [description](#)
+  * [installation](#installation)
+  * [usage](#usage)
+  * [contributing](#contributing)
+  * [tests](#tests)
+  * [email](#email)
+  * [github](#github)
+  * [license](#license)
+      
+  ## installation
+  ${data.Installation}
+  ## usage
+  ${data.Usage}node
+  ## contributing
+  ${data.Contributing}
+  ## tests
+  ${data.Tests}
+  ## email
+  ${data.enterYourEmail}
+  ## github
+  ${data.enterGitHubUsername}
+  ## license
+  ${data.License} 
+  
 `;
 }
 
